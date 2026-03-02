9ad64d8e4971ca8e6cc07064e07b8959e17e1e8d
rdavidboucher <54682986+rdavidboucher@users.noreply.github.com>
2026-03-02 12:06:22 -0500

    Add files via upload

--- aporia-project/web-app/src/main.jsx
+++ aporia-project/web-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' // This looks for the 'export default' we just added
-import './index.css'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
